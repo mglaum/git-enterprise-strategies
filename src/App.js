cbe5e0c01fedc2fb9eca414363f77188cfa1661b
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:28:41 -0500

    Handled merge conflics with merging newText into main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './gator.gif';
 import './App.css';
 
 function App() {
@@ -9,6 +9,9 @@ function App() {
         <p>
           Hello World! This is our team project.
         </p>	
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Madeleine Glaum and this is my project </p>
       </header>
     </div>
   );
